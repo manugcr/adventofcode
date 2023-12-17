a5b1ff0ca8db5e49327d14dacb3ad5336cfb190e
manugcr <mgilcernich@gmail.com>
2023-12-17 15:34:30 -0300

    day 7

--- day_7/main.py
+++ day_7/main.py
@@ -1,43 +1,51 @@
+# 32T3K 765
+# T55J5 684
+# KK677 28
+# KTJJT 220
+# QQQJA 483
+
 print('--- Day 7: Camel Cards ---')
 
-with open('input.txt') as file:
-    lines = file.read().strip().split('\n')
+# with open('input.txt', 'r') as file:
+#     lines = file.read().strip().split('\n')
 
-# Every hand is exactly one type. From strongest to weakest, they are:
+# def value(card):
+#     return '23456789TJQKA'.index(card)
 
-#     Five of a kind, where all five cards have the same label: AAAAA
-#     Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-#     Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-#     Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-#     Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-#     One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-#     High card, where all cards' labels are distinct: 23456
-def get_type(hand):
-    # Five of a kind
-    if hand[0] == hand[1] == hand[2] == hand[3] == hand[4]:
-        return 'Five of a kind'
-    # Four of a kind
-    elif hand[0] == hand[1] == hand[3] == hand[4] or hand[1] == hand[2] == hand[3] == hand[4]:
-        return 'Four of a kind'
-    # Full house
-    elif hand[0] == hand[1] == hand[2] and hand[3] == hand[4] or hand[0] == hand[1] and hand[2] == hand[3] == hand[4]:
-        return 'Full house'
-    # Three of a kind
-    elif hand[0] == hand[1] == hand[2] or hand[1] == hand[2] == hand[3] or hand[2] == hand[3] == hand[4]:
-        return 'Three of a kind'
-    # Two pair
-    elif hand[0] == hand[1] and hand[2] == hand[3] or hand[0] == hand[1] and hand[3] == hand[4] or hand[1] == hand[2] and hand[3] == hand[4]:
-        return 'Two pair'
-    # One pair
-    elif hand[0] == hand[1] or hand[1] == hand[2] or hand[2] == hand[3] or hand[3] == hand[4]:
-        return 'One pair'
-    # High card
-    else:
-        return 'High card'
+# def get_count(hand):
+#     counts = {}
+#     for card in hand:
+#         if card not in counts:
+#             counts[card] = 0
+#         counts[card] += 1
+#     return sorted(counts.values(), reverse=True)
 
+# def tiebreak(hand):
+#     sc = hand
+#     sc.extend([value(x) for x in hand])
+#     return sc
 
-for i, line in enumerate(lines):
-    hand = line.split(' ')[0]
-    bid = line.split(' ')[1]
+# hands = []
+# for line in lines:
+#     hand, bid = line.split(' ')
+#     hands.append((tiebreak(hand), hand, int(bid)))
+# hands.sort()
 
-    print(hand, bid)
+import collections
+lines = [i for i in open('input.txt').read().split('\n') if i.strip()]
+def hand(h,part1):
+    if part1: h = h.replace('J', 'X')
+    h2 = ['J23456789TXQKA'.index(i)for i in h]
+    ts = []
+    for r in '23456789TQKA':
+        c = collections.Counter(h.replace('J', r))
+        p = tuple(sorted(c.values()))
+        t = [(1,1,1,1,1),(1,1,1,2),(1,2,2),(1,1,3),(2,3),(1,4),(5,)].index(p)
+        ts.append(t)
+    return (max(ts), *h2)
+for part1 in (True, False):
+    h = sorted((hand(h,part1), int(b)) for h, b in (l.split() for l in lines))
+    t = 0
+    for i,(_,b) in enumerate(h):
+        t+=i*b+b
+    print('Part', 2-part1, ':', t)
